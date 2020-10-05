a56d8c30ea3007a194d017e07dc02abce1c4d857
thakurgouravsingh <gouravsinght3@gmail.com>
2020-10-06 00:21:06 +0530

    updated forloops

--- forloops.java
+++ forloops.java
@@ -1,7 +1,21 @@
 public class Record{
-    public void getRecord(int arr[]){
-        for(int rec=0;rec<arr.length;rec++){
+    private int total=0;
+    public void getRecord(int arr[])
+    {
+        for(int rec=0;rec<arr.length;rec++)
+        {
              total=arr[rec]+total;
         }
+      
     }
+     public int Total()
+        {
+            return total;
+        }
+        public static void main(String args[])
+        {   int arr[]={2,3,4,5};
+            Record rec=new Record();
+            rec.getRecord(arr);
+            System.out.println(rec.Total());
+        }
 }
